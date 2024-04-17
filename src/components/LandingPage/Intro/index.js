d20ef372fed61c02ab677348b28a4bcfb6ce4094
Adarsh Pandey <meadarshpandey@gmail.com>
2024-04-17 09:42:46 +0530

    version1.0

--- src/components/LandingPage/Intro/index.js
+++ src/components/LandingPage/Intro/index.js
@@ -55,7 +55,7 @@ function LandingPageComponent() {
           <RWebShare
             data={{
               text: "Crypto Dashboard made using React JS.",
-              url: "https://crypto-dashboard-dec.netlify.app/",
+              url: "https://crypto-tracker-webapp.vercel.app/",
               title: "CryptoDashboard.",
             }}
             onClick={() => console.log("shared successfully!")}
